48e8cd628183b3b556f4f1aa6758ea9f35ae9df1
Maryam <72814017+Maryam-Nsi@users.noreply.github.com>
2023-04-20 19:44:01 +0330

    add login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,59 @@
+import React, {useState} from 'react';
+import './Login.css'
+
+import { useLogin } from '../hooks/useLogin'
+
 function Login() {
+    const {login, isPending, error} = useLogin()
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+  
+  
+    const clearForm = () => {
+      setUsername('')
+      setPassword('')
+    }
+  
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        login(username, password)
+        clearForm()
+    }
     return ( <>
+    <div className="auth-wrapper login">
+      <div className="auth-inner">
+        <form onSubmit={handleSubmit}>
+          <h3 className='fw-bold'>ورود</h3>
+          <div className="mb-3">
+            <label className='fw-bold'>نام کاربری</label>
+            <input type="text" className="form-control" placeholder="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              />
+          </div>
+          <div className="mb-3">
+            <label className='fw-bold'>رمزعبور</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              />
+          </div>
+          <div className="d-grid mt-4 fw-bold">
+            <button type="submit" className="btn btn-primary">
+              ورود
+            </button>
+          </div>
+          {error && <p className="btn btn-danger mt-2">
+            {error}
+          </p>}
+        </form>
+      </div>
+    </div>
     </> );
 }
 
